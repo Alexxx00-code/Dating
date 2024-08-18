8d825f22bbdec88dea07a1680dd3d7a534bdfcc4
Alexxx00-code <sascha-cherepenin@mail.ru>
2024-08-18 20:05:18 +0400

    Refactoring User secrets

--- DatingTelegramBot/Program.cs
+++ DatingTelegramBot/Program.cs
@@ -1,109 +1,38 @@
-﻿using Telegram.Bot.Polling;
-using Telegram.Bot.Types.Enums;
-using Telegram.Bot;
-using Telegram.Bot.Exceptions;
-using Telegram.Bot.Types;
-using System;
-using Telegram.Bot.Types.ReplyMarkups;
+﻿using Microsoft.Extensions.Configuration;
+using DatingTelegramBot;
 
 class Program
 {
-    static async Task Main()
+    static async Task Main(string[] args)
     {
-        var _botClient = new TelegramBotClient("7544862344:AAGpAIzdLAe0oAZ2fyczCrRZd-eHcx0_kuY"); // Присваиваем нашей переменной значение, в параметре передаем Token, полученный от BotFather
-        var _receiverOptions = new ReceiverOptions // Также присваем значение настройкам бота
+        var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+        var botToken = config["TelegramBotToken"];
+        var url = System.Configuration.ConfigurationManager.AppSettings["Url"];
+        switch (args.Length)
         {
-            AllowedUpdates = new[] // Тут указываем типы получаемых Update`ов, о них подробнее расказано тут https://core.telegram.org/bots/api#update
-            {
-                UpdateType.Message, // Сообщения (текст, фото/видео, голосовые/видео сообщения и т.д.)
-            },
-            // Параметр, отвечающий за обработку сообщений, пришедших за то время, когда ваш бот был оффлайн
-            // True - не обрабатывать, False (стоит по умолчанию) - обрабаывать
-            ThrowPendingUpdates = true,
-        };
+            case 1:
+                {
+                    url = args[0];
+                    break;
+                }
+
+            case 2:
+                {
+                    url = args[0];
+                    botToken = args[1];
+                    break;
+                }
+        }
+
+        DatingBot datingBot = new DatingBot(botToken, url, i => Console.WriteLine(i));
 
         using var cts = new CancellationTokenSource();
 
-        // UpdateHander - обработчик приходящих Update`ов
-        // ErrorHandler - обработчик ошибок, связанных с Bot API
-        _botClient.StartReceiving(UpdateHandler, ErrorHandler, _receiverOptions, cts.Token); // Запускаем бота
-
-        var me = await _botClient.GetMeAsync(); // Создаем переменную, в которую помещаем информацию о нашем боте.
-        Console.WriteLine($"{me.FirstName} запущен!");
+        await datingBot.Run(cts.Token);
 
         Console.WriteLine("Press Enter to exit...");
         Console.ReadLine();
+
         cts.Cancel();
-    }
-
-    private static async Task UpdateHandler(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
-    {
-        // Обязательно ставим блок try-catch, чтобы наш бот не "падал" в случае каких-либо ошибок
-        try
-        {
-            // Сразу же ставим конструкцию switch, чтобы обрабатывать приходящие Update
-            switch (update.Type)
-            {
-                case UpdateType.Message:
-                    {
-                        // эта переменная будет содержать в себе все связанное с сообщениями
-                        var message = update.Message;
-
-                        // From - это от кого пришло сообщение (или любой другой Update)
-                        var user = message.From;
-
-                        // Chat - содержит всю информацию о чате
-                        var chat = message.Chat;
-
-                        // Выводим на экран то, что пишут нашему боту, а также небольшую информацию об отправителе
-                        Console.WriteLine($"{user.FirstName} ({user.Id}) написал сообщение: {message.Text}");
-
-                        WebAppInfo webAppInfo = new WebAppInfo() { Url = "https://69fe-80-237-123-73.ngrok-free.app" };
-
-                        if (message.Text == "/start")
-                        {
-                            // Тут создаем нашу клавиатуру
-                            var inlineKeyboard = new InlineKeyboardMarkup(
-                                new List<InlineKeyboardButton[]>() // здесь создаем лист (массив), который содрежит в себе массив из класса кнопок
-                                {
-                                        // Каждый новый массив - это дополнительные строки,
-                                        // а каждая дополнительная строка (кнопка) в массиве - это добавление ряда
-
-                                        new InlineKeyboardButton[] // тут создаем массив кнопок
-                                        {
-                                            InlineKeyboardButton.WithWebApp("Это кнопка с сайтом", webAppInfo)
-                                        },
-                                });
-
-                            await botClient.SendTextMessageAsync(
-                                chat.Id,
-                                "Это inline клавиатура!",
-                                replyMarkup: inlineKeyboard); // Все клавиатуры передаются в параметр replyMarkup
-
-                            return;
-                        }
-
-                        return;
-                    }
-            }
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.ToString());
-        }
-    }
-
-    private static Task ErrorHandler(ITelegramBotClient botClient, Exception error, CancellationToken cancellationToken)
-    {
-        // Тут создадим переменную, в которую поместим код ошибки и её сообщение 
-        var ErrorMessage = error switch
-        {
-            ApiRequestException apiRequestException
-                => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
-            _ => error.ToString()
-        };
-
-        Console.WriteLine(ErrorMessage);
-        return Task.CompletedTask;
     }
 }
