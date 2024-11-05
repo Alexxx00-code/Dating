3ca2bc0d453b46435a25b500bf96d403b05fe44e
Alexxx00-code <sascha-cherepenin@mail.ru>
2024-11-05 19:03:55 +0400

    Added DictionaryController

---
+++ Dating.Api/Controllers/DictionaryController.cs
@@ -0,0 +1,113 @@
+﻿using Dating.Api.Models;
+using Dating.Aplication.Interfaces;
+using Dating.Aplication.Models;
+using Dating.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dating.Api.Controllers
+{
+    public abstract class DictionaryController : ControllerBase
+    {
+        private readonly IParameterService<Gender> genderService;
+        private readonly IParameterService<City> cityService;
+        private readonly IParameterService<EyesColor> eyesColorService;
+        private readonly IParameterService<HairColor> hairColorService;
+        private readonly IParameterService<Language> languageService;
+        private readonly IParameterService<SexOrientation> sexOrientationService;
+        private readonly IParameterService<Tag> tagService;
+        private readonly IParameterService<ZodiacSign> zodiacSignService;
+
+        public DictionaryController(
+            IParameterService<Gender> genderService,
+            IParameterService<City> cityService,
+            IParameterService<EyesColor> eyesColorService,
+            IParameterService<HairColor> hairColorService,
+            IParameterService<Language> languageService,
+            IParameterService<SexOrientation> sexOrientationService,
+            IParameterService<Tag> tagService,
+            IParameterService<ZodiacSign> zodiacSignService)
+        {
+            this.genderService = genderService;
+            this.cityService = cityService;
+            this.eyesColorService = eyesColorService;
+            this.hairColorService = hairColorService;
+            this.languageService = languageService;
+            this.sexOrientationService = sexOrientationService;
+            this.tagService = tagService;
+            this.zodiacSignService = zodiacSignService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<DictionaryModel>> GetList()
+        {
+            DictionaryModel model = new DictionaryModel {
+                Cities = await cityService.GetList(),
+                EyesColors = await eyesColorService.GetList(),
+                Genders = await hairColorService.GetList(),
+                Tags = await tagService.GetList(),
+                HairColors = await hairColorService.GetList(),
+                Languages = await languageService.GetList(),
+                SexOrientations = await sexOrientationService.GetList(),
+                ZodiacSigns = await zodiacSignService.GetList(),
+            };
+
+            return Ok(model);
+        }
+    }
+    /*
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GenderController : DictionaryController<Gender>
+    {
+        public GenderController(IParameterService<Gender> parameterService) : base(parameterService) { }
+    }
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CityController : DictionaryController<City>
+    {
+        public CityController(IParameterService<City> parameterService) : base(parameterService) { }
+    }
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EyesColorController : DictionaryController<EyesColor>
+    {
+        public EyesColorController(IParameterService<EyesColor> parameterService) : base(parameterService) { }
+    }
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HairColorController : DictionaryController<HairColor>
+    {
+        public HairColorController(IParameterService<HairColor> parameterService) : base(parameterService) { }
+    }
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LanguageController : DictionaryController<Language>
+    {
+        public LanguageController(IParameterService<Language> parameterService) : base(parameterService) { }
+    }
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SexOrientationController : DictionaryController<SexOrientation>
+    {
+        public SexOrientationController(IParameterService<SexOrientation> parameterService) : base(parameterService) { }
+    }
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TagController : DictionaryController<Tag>
+    {
+        public TagController(IParameterService<Tag> parameterService) : base(parameterService) { }
+    }
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ZodiacSignController : DictionaryController<ZodiacSign>
+    {
+        public ZodiacSignController(IParameterService<ZodiacSign> parameterService) : base(parameterService) { }
+    }*/
+}
---
+++ Dating.Api/Models/DictionaryModel.cs
@@ -0,0 +1,17 @@
+﻿using Dating.Aplication.Models;
+using Dating.Domain.Models;
+
+namespace Dating.Api.Models
+{
+    public class DictionaryModel
+    {
+        public List<ParameterModel> Genders { get; set; }
+        public List<ParameterModel> Cities { get; set; }
+        public List<ParameterModel> EyesColors { get; set; }
+        public List<ParameterModel> HairColors { get; set; }
+        public List<ParameterModel> Languages { get; set; }
+        public List<ParameterModel> SexOrientations { get; set; }
+        public List<ParameterModel> Tags { get; set; }
+        public List<ParameterModel> ZodiacSigns { get; set; }
+    }
+}
